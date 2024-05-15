c094d64d5ede5e80425b211f93ffc94e47e72b1d
alix2018 <stephanie.alix95@gmail.com>
2024-05-15 12:54:00 +0200

    Improve logic of lists creation

--- src/views/ListCreate.vue
+++ src/views/ListCreate.vue
@@ -2,20 +2,68 @@
 import { computed, type Ref, ref } from 'vue';
 import MainHeader from '@/components/MainHeader.vue';
 import { useRouter } from 'vue-router';
+import type { Word } from '@/types';
+import type { UUID } from 'crypto';
 
 const router = useRouter();
 
+// TODO: temporary until the original Word type is fixed
+type wordsArrayType = Array<Word & { id: UUID }>;
 const listTitle: Ref<string> = ref('');
 const valid: Ref<boolean | null | undefined> = ref(null);
 const rules: Ref<{
-  required: boolean | string;
+  titleRequired: boolean | string;
 }> = computed(() => {
   return {
-    required: !!listTitle.value || 'Field is required'
+    titleRequired: !!listTitle.value || 'Field is required'
   };
 });
+const inputsArray: Ref<wordsArrayType> = ref([
+  { id: crypto.randomUUID(), source: '', translation: '' }
+]);
+
+const hasAtLeastOneEntry = computed(() => {
+  const hasOneItem = inputsArray.value.length === 1;
+  const isSourceEmpty = inputsArray.value[0].source === '';
+  const isTranslationEmpty = inputsArray.value[0].translation === '';
+  if (hasOneItem && (isSourceEmpty || isTranslationEmpty)) {
+    return false;
+  } else {
+    return true;
+  }
+});
+
+const isLastRowEmpty = computed(() => {
+  const inputsArrayLength = inputsArray.value.length;
+  const isSourceEmpty = inputsArray.value[inputsArrayLength - 1].source === '';
+  const isTranslationEmpty = inputsArray.value[inputsArrayLength - 1].translation === '';
+  return isSourceEmpty && isTranslationEmpty;
+});
+
+function onClickRemoveRow(index: number) {
+  inputsArray.value.splice(index, 1);
+}
+
+// TODO: Be able to at x rows in once
+function onClickAddRow() {
+  inputsArray.value.push({ id: crypto.randomUUID(), source: '', translation: '' });
+}
+
+function formatArray(inputsArray: wordsArrayType) {
+  const resArray = [];
+  for (const input of inputsArray) {
+    if (input.source !== '' && input.translation !== '') {
+      resArray.push(input);
+    }
+  }
+
+  return resArray;
+}
 
 function onClickCreateList() {
+  const formattedArray = formatArray(inputsArray.value);
+  console.log('formattedArray', formattedArray);
+  // TODO: Call POST endpoint to create a list
   router.push({ name: 'lists' });
 }
 </script>
@@ -25,7 +73,7 @@ function onClickCreateList() {
 
   <v-form v-model="valid" class="form">
     <v-text-field
-      :rules="[rules.required]"
+      :rules="[rules.titleRequired]"
       class="list-title"
       label="List title"
       variant="underlined"
@@ -33,25 +81,43 @@ function onClickCreateList() {
       clearable
     />
 
-    <v-table class="table" density="comfortable" fixed-header>
+    <v-table class="table" density="compact" fixed-header>
       <thead class="thead">
         <tr>
           <th class="th">Source</th>
           <th class="th">Translation</th>
+          <th class="th last-th"></th>
         </tr>
       </thead>
       <tbody>
-        <tr>
+        <tr v-for="(input, index) in inputsArray" :key="index">
           <td>
-            <v-text-field class="input" variant="underlined" />
+            <v-text-field variant="underlined" v-model="input.source" />
           </td>
-          <td><v-text-field class="input" variant="underlined" /></td>
+          <td>
+            <v-text-field variant="underlined" v-model="input.translation" />
+          </td>
+          <td>
+            <!-- Add validation for empty inputs -->
+            <v-btn-text
+              class="btn-remove"
+              icon="mdi-delete-outline"
+              @click="onClickRemoveRow(index)"
+            />
+          </td>
+        </tr>
+        <tr class="btn-add-container">
+          <v-btn-text class="btn-add" @click="onClickAddRow" :disabled="isLastRowEmpty">
+            + Add
+          </v-btn-text>
         </tr>
       </tbody>
     </v-table>
 
     <!-- TODO: Compute valid property -->
-    <v-btn class="btn-create" @click="onClickCreateList" :disabled="!valid"> Create </v-btn>
+    <v-btn class="btn-create" @click="onClickCreateList" :disabled="!valid || !hasAtLeastOneEntry">
+      Create
+    </v-btn>
   </v-form>
 </template>
 
@@ -75,6 +141,21 @@ function onClickCreateList() {
 
 .th {
   font-weight: bold !important;
+}
+
+.last-th {
+  width: 20px !important;
+}
+
+.btn-add-container {
+  position: relative;
+  height: 45px;
+}
+
+.btn-add {
+  position: absolute;
+  right: 10px;
+  margin-top: 5px;
 }
 
 .btn-create {
